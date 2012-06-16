3773a17c4e676ce75cecd04dc072cbad85f7e06f
Matteo Micheletti <kingletti88@gmail.com>
2012-06-16 16:42:50 +0200

    Improved the quality of the outputted graph. The result is still not so good, but it is better than before. Further improvements will eventually come :).

--- connettere/connect/management/commands/makeGraph.py
+++ connettere/connect/management/commands/makeGraph.py
@@ -16,7 +16,13 @@ class Command(BaseCommand):
 
 
     def makeGraph(self, filename):
-        mygraph = pydot.Dot(graph_type='graph')
+        """ rankdir: orientation of the graph
+            graph_type: directed, non-directed
+            nodesep: minimum vertical distance between adjacent ranks of nodes
+            ranksep: minimum horizontal distance between adjacent nodes of equal rank
+    """
+        mygraph = pydot.Dot(rankdir='LR',graph_type='digraph',nodesep='1',ranksep='20')
+        #mygraph = pydot.Dot(graph_type='graph')
         
         thing_nodes = []
         i = 0
@@ -25,20 +31,29 @@ class Command(BaseCommand):
         color = 100000
 
         for thing in AThing.objects.all():
-            thing_nodes.insert(i,pydot.Node(thing.__unicode__().replace("::"," "), style="filled", fillcolor="green"))
+            t_node = pydot.Node(thing.__unicode__().replace("::"," ").replace("\"",""), style="filled", fillcolor="green")
+            thing_nodes.insert(i,t_node)
+            mygraph.add_node(t_node)
+            #print "add thing node: %s" % (t_node.get_name())
             i = i + 1
 
+        #print "\n"
+
         for group in AGroup.objects.all():
+            #print "\n"
             color += hop
-            group_node = pydot.Node(group.__unicode__(), style="filled", fillcolor="red")
+            group_node = pydot.Node(group.__unicode__().replace("\"",""), style="filled", fillcolor="red")
+            mygraph.add_node(group_node)
+            #print "add group node: %s" % (group_node.get_name())
             for thing in group.thing_set.all():
                 for thing_node in thing_nodes:
                     #print s + thing.__unicode__().replace("::"," ") + s
                     #print thing_node.get_name()
                     if (s + thing.__unicode__().replace("::"," ") + s) == thing_node.get_name():
-                        print '#' + str(color)
+                        #print '#' + str(color)
                         edge = pydot.Edge(group_node, thing_node,color='#' + str(color))
                         mygraph.add_edge(edge)
+                        #print "add edge from: %s to: %s" % (group_node.get_name(),thing_node.get_name())
                         break
 
         mygraph.write_png(filename)
